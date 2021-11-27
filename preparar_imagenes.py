de25512c2ea479ed089c1326365311de0151c501
AbeJLazaro <abrahamlazaro@comunidad.unam.mx>
2021-11-27 10:09:41 -0600

    Función para redimensionar y formatear imagenes

---
+++ preparar_imagenes.py
@@ -0,0 +1,23 @@
+import os 
+import cv2
+import numpy as np
+
+# Cambiar esta URL
+url_git = "C:/Users/Usuario/Documents/SIAFI/GANs/git/michiGAN"
+
+# 
+carpetas = os.listdir(os.path.join(url_git,"imagenes"))
+
+for carpeta in carpetas:
+    url_carpeta_origen = os.path.join(url_git,"imagenes",carpeta)
+    url_carpeta_destino = os.path.join(url_git,"imagenesListas",carpeta)
+    if not os.path.exists(url_carpeta_destino):
+        os.mkdir(url_carpeta_destino)
+    for archivo in os.listdir(url_carpeta_origen):
+        img = cv2.imread(os.path.join(url_carpeta_origen,archivo))
+        img = img[:,:,:3]
+        res = cv2.resize(img, dsize=(128, 128), interpolation=cv2.INTER_CUBIC)
+        nombre = archivo.split(".")[0]
+        nombre = nombre+".png"
+        cv2.imwrite(os.path.join(url_carpeta_destino,nombre), res)
+        
