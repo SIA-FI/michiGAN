e1f1d23d967307ca583d2ef68c6d4f28aa6d8256
AbeJLazaro <abrahamlazaro@comunidad.unam.mx>
2021-11-27 10:11:21 -0600

    solo preprocesa las imagenes sin carpeta en imagenesListas

--- preparar_imagenes.py
+++ preparar_imagenes.py
@@ -13,11 +13,11 @@ for carpeta in carpetas:
     url_carpeta_destino = os.path.join(url_git,"imagenesListas",carpeta)
     if not os.path.exists(url_carpeta_destino):
         os.mkdir(url_carpeta_destino)
-    for archivo in os.listdir(url_carpeta_origen):
-        img = cv2.imread(os.path.join(url_carpeta_origen,archivo))
-        img = img[:,:,:3]
-        res = cv2.resize(img, dsize=(128, 128), interpolation=cv2.INTER_CUBIC)
-        nombre = archivo.split(".")[0]
-        nombre = nombre+".png"
-        cv2.imwrite(os.path.join(url_carpeta_destino,nombre), res)
+        for archivo in os.listdir(url_carpeta_origen):
+            img = cv2.imread(os.path.join(url_carpeta_origen,archivo))
+            img = img[:,:,:3]
+            res = cv2.resize(img, dsize=(128, 128), interpolation=cv2.INTER_CUBIC)
+            nombre = archivo.split(".")[0]
+            nombre = nombre+".png"
+            cv2.imwrite(os.path.join(url_carpeta_destino,nombre), res)
         
